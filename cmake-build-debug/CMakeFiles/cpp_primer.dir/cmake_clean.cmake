847f08cb9cefa1e5a7d9b838e0da3dcced841104
hua_shuo <1970323510@qq.com>
2019-07-05 20:50:03 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/cpp_primer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_primer.dir/main.cpp.o"
+  "cpp_primer.pdb"
+  "cpp_primer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_primer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/1/main_only.cc.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
